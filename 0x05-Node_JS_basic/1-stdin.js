f643ad9bf81935dad36ea6e643a050079fad431a
afialisa <afianourish72@gmail.com>
2024-09-18 15:54:09 +0100

    nodejs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,14 @@
 const readline = require('readline');
-
-// Create an interface for input/output
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-// Display the welcome message
 console.log("Welcome to Holberton School, what is your name?");
-
-// On receiving input, display the name
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
-  
-  // Close the readline interface when input ends
   rl.close();
 });
-
-// On closing the readline interface
 rl.on('close', () => {
   console.log("This important software is now closing");
 });
-
-module.exports = { rl };  // Exporting for potential testing
+module.exports = { rl };
